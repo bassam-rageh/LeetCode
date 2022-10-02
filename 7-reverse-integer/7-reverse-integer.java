90c7373943c7edf6156cde503fa7a588dd7126c0
Bassam Rageh <48591675+bassam-rageh@users.noreply.github.com>
2022-10-02 22:02:12 +0200

    Time: 2 ms (81.04%), Space: 40.8 MB (82.68%) - LeetHub

---
+++ 7-reverse-integer/7-reverse-integer.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int reverse(int x) {
+        long ans = 0;
+        while(x!=0){
+          ans *= 10;
+          ans += x%10;
+          x /= 10;
+          if(ans>=Integer.MAX_VALUE || ans<=-Integer.MAX_VALUE){
+            return 0;
+          }
+        }
+        return (int) ans;
+    }
+}
