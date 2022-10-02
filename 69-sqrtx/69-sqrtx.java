653e9f17cfa9338e96aef6f535dbffc00819eb15
Bassam Rageh <48591675+bassam-rageh@users.noreply.github.com>
2022-10-02 22:44:58 +0200

    Time: 3 ms (44.94%), Space: 40.9 MB (77.06%) - LeetHub

--- 69-sqrtx/69-sqrtx.java
+++ 69-sqrtx/69-sqrtx.java
@@ -1,8 +1,5 @@
 class Solution {
     public int mySqrt(int x) {
-        if (x == 0) {
-            return 0;
-        }
         long a = 1, b = x;
         long sq = (a + b) / 2;
         while (a < b) {
