715a23de40159719fa11c89ff4c952021dd8af7f
Bassam Rageh <48591675+bassam-rageh@users.noreply.github.com>
2022-10-03 13:05:47 +0200

    Time: 25 ms (33.19%), Space: 70.5 MB (35.91%) - LeetHub

---
+++ 217-contains-duplicate/217-contains-duplicate.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> map = new HashSet<Integer>(nums.length);
+        for (int i : nums) {
+            if (map.contains(i))
+                return true;
+            map.add(i);
+        }
+        return false;
+    }
+}
