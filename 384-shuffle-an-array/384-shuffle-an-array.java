bb478139c3ae79259d75eecbb7f7a2ee3d8cbad8
Bassam Rageh <48591675+bassam-rageh@users.noreply.github.com>
2022-10-03 13:55:25 +0200

    Time: 124 ms (27.07%), Space: 65.9 MB (57.50%) - LeetHub

---
+++ 384-shuffle-an-array/384-shuffle-an-array.java
@@ -0,0 +1,32 @@
+class Solution {
+    int[] arr;
+    int[] shuffled;
+
+    public Solution(int[] nums) {
+        arr = nums;
+        shuffled = nums.clone();
+    }
+
+    public int[] reset() {
+        for (int i = 0; i < shuffled.length; i++) {
+            shuffled[i] = arr[i];
+        }
+        return shuffled;
+    }
+
+    public int[] shuffle() {
+        for (int i = 0; i < shuffled.length; i++) {
+            int random = new Random().nextInt(shuffled.length);
+            int temp = shuffled[i];
+            shuffled[i] = shuffled[random];
+            shuffled[random] = temp;
+        }
+        return shuffled;
+    }
+}
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution obj = new Solution(nums);
+ * int[] param_1 = obj.reset();
+ * int[] param_2 = obj.shuffle();
+ */
